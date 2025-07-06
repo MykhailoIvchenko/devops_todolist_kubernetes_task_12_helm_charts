dc2c21e5e0b75ed1cd8b4425995a3e9d8df76bee
Mykhailo <ivchenkomykhailo@gmail.com>
2025-07-06 15:56:14 +0300

    Fix according to the comments

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,10 @@ NAMESPACE="todoapp"
 kubectl create namespace "$NAMESPACE" --dry-run=client -o yaml | kubectl apply -f -
 
 # Update Helm dependencies (mysql subchart)
-helm dependency update ./helm-chart
+helm dependency update ./helm-chart/todoapp
 
 # Install or upgrade todoapp Helm chart (with mysql subchart)
-helm upgrade --install todoapp ./helm-chart --namespace "$NAMESPACE" --wait
+helm upgrade --install todoapp ./helm-chart/todoapp --namespace "$NAMESPACE" --wait
 
 # Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
